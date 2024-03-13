96f7837f8cee2fe206d28ace7a7dbbf5c7236fd1
MihuGhost <luckyjojok@gmail.com>
2024-03-13 10:57:23 +0800

    226 二叉树交换--递归、迭代

---
+++ Leetcode/226/main.go
@@ -0,0 +1,41 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+
+}
+
+// 递归 前中后序 都可以实现
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left, root.Right = root.Right, root.Left
+	invertTree(root.Left)
+	invertTree(root.Right)
+	return root
+}
+
+// 迭代前序遍历 栈实现
+func invertTree1(root *TreeNode) *TreeNode {
+	var stack []*TreeNode
+
+	node := root
+	for node != nil || len(stack) > 0 {
+		for node != nil {
+			node.Left, node.Right = node.Right, node.Left
+			stack = append(stack, node)
+			node = node.Left
+		}
+		node = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		node = node.Right
+	}
+
+	return root
+}
